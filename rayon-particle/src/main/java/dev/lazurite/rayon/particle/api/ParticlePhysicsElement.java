8aa4d279640f6683244013edbdcc1011b10dd24e
Marco Rebhan <me@dblsaiko.net>
2021-06-18 14:04:11 +0200

    Fix compile issue

--- rayon-particle/src/main/java/dev/lazurite/rayon/particle/api/ParticlePhysicsElement.java
+++ rayon-particle/src/main/java/dev/lazurite/rayon/particle/api/ParticlePhysicsElement.java
@@ -1,10 +1,13 @@
 package dev.lazurite.rayon.particle.api;
 
+import java.util.Collections;
+
 import dev.lazurite.rayon.core.api.PhysicsElement;
+import dev.lazurite.rayon.core.impl.bullet.collision.body.shape.MinecraftShape;
 
 public interface ParticlePhysicsElement extends PhysicsElement {
     @Override
-    default void reset() {
-
+    default MinecraftShape genShape() {
+        return new MinecraftShape(Collections.emptyList());
     }
 }
